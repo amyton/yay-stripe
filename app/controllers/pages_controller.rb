fedf5c4a37cab069709499e94d375c2277d6debe
Amy Ton <amyhton@gmail.com>
2014-08-13 22:43:24 -0700

    Add new home page

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,6 @@
+# PagesController will inherit from ApplicationController
+# If a PagesController doesn't know what to do, the ApplicationController will
+class PagesController < ApplicationController
+	def home
+	end
+end
