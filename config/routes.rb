99f624fb9fe9d311361ba62d052ecdff24e87067
Amy Ton <amyhton@gmail.com>
2014-08-15 12:34:34 -0700

    Add comment for future reference

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  # Run the pages controller and calling the home method
   root "pages#home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
